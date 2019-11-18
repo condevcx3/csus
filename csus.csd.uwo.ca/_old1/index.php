f70fda2a1490e3868df84cee019697787ee4702f
Connor Cozens <ccozens2@uwo.ca>
2019-11-18 13:55:21 -0500

    Initial Website Data Upload

---
+++ csus.csd.uwo.ca/_old1/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSUS</title>
+	<meta charset="UTF-8" />
+	<base target="_blank" />
+	<link rel="stylesheet" href="css/csus.min.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="js/csus.min.js" async></script>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header>
+		<h1>CSUS</h1>
+		<ul id="header-nav">
+			<li><a href="#intro" target="_self" class="smooth">Intro</a></li>
+			<li><a href="#events" target="_self" class="smooth">Events</a></li>
+			<li><a href="#documents" target="_self" class="smooth">Documents</a></li>
+			<li><a href="#execs" target="_self" class="smooth">Executives</a></li>
+			<li><a href="#contact" target="_self" class="smooth">Contact</a></li>
+		</ul>
+	</header>
+	<div id="entire-body">
+		<div class="fullwidth-container">
+			<img class="fullwidth" src="images/middlesex.jpg" />
+		</div>
+		<div class="container">
+			<section id="intro">
+				<h1>Introduction</h1>
+				<p>The Computer Science Undergraduate Society is the organization that represents all computer science students at <a href="http://uwo.ca">Western University</a> in London, Ontario. As the oldest and most established computer science organization on campus, CSUS is run by students and for students. We work closely with the <a href="http://www.csd.uwo.ca/">Department of Computer Science</a> to promote computer science as an academic discipline, to connect students to related university services, and to provide methods of networking between students, faculty, and employers.</p>
+			</section>
+			<section id="events">
+				<h1>Events</h1>
+				<p>Throughout the year, CSUS runs a number of events for students, bridging the gap between the academic world and social life. In past years, we have organized events both on and off campus, including:</p>
+				<ul>
+					<li>Laser Tag</li>
+					<li>Bowling Night</li>
+					<li>Pub Nights</li>
+					<li>Exam prep and review</li>
+				</ul>
+				<p>By far, our most popular event is our annual Academic Colloquium, where students gather for an evening of talks presented by the department's leading researchers. Be sure to keep in touch with us to find out what we'll be doing next!</p>
+			</section>
+			<section id="documents">
+				<h1>Documents</h1>
+				<p>Here's some files that we've used over the year:</p>
+				<p><a href="docs/csus_public_meeting_1.pdf">Public Meeting #1</a></p>
+			</section>
+			<section id="execs">
+				<h1>Executives</h1>
+				<div class="exec-container">
+					<div class="exec">
+						<div class="exec-image-container"><img src="images/kyle.png" class="exec-image" /></div>
+						<h1>Kyle Windsor</h1>
+						<p class="exec-detail"><b>President</b><br />5th Year CS/Astrophysics<br /><a href="mailto:kwindsor@uwo.ca">kwindsor@uwo.ca</a></p>
+						<p>Hi! My name's Kyle Windsor, and I'm the President of CSUS. I was a student in the Department of Physics for my first through third years, before earning a minor and switching into Computer Science. I'm currently in my 5th year at Western, and my 3rd year of CS.<br /><br />
+						
+						Feel free to contact me at any time through my university e-mail!</p>
+					</div>
+				</div>
+				<div class="exec-container">
+					<div class="exec">
+						<div class="exec-image-container"><img src="images/kaitlin.png" class="exec-image" /></div>
+						<h1>Kaitlin Lem</h1>
+						<p class="exec-detail"><b>VP Social</b><br />3rd Year BMOS<br /><a href="mailto:klem@uwo.ca">klem@uwo.ca</a></p>
+						<p>Hey guys! My name’s Kaitlin Lem and I’m the VP Social of CSUS. I’m currently in BMOS studying accounting but computer sciences continue to be a passion. Look forward to special events like bowling, pub nights, and lasertag throughout the year! If you have any suggestions please email me!</p>
+					</div>
+				</div>
+				<div class="exec-container">
+					<div class="exec">
+						<div class="exec-image-container"><img src="images/sarah.png" class="exec-image" /></div>
+						<h1>Sarah Clowes-Walker</h1>
+						<p class="exec-detail"><b>VP Communications</b><br />2nd Year BMOS<br /><a href="mailto:sclowesw@uwo.ca">sclowesw@uwo.ca</a></p>
+						<p>Hey, I'm Sarah Clowes-Walker and I'm the VP of Communications for CSUS this year! I was the First Year Representative last year and I'm excited to continue my involvement now that I'm in my second year at Western. Don't hesitate to send me an email, I would love to answer any questions or hear your suggestions!</p>
+					</div>
+				</div>
+				<div class="exec-container">
+					<div class="exec">
+						<div class="exec-image-container"><img src="images/killian.jpg" class="exec-image" /></div>
+						<h1>Killian McGuinness</h1>
+						<p class="exec-detail"><b>VP Academic</b><br />4th Year CS/Applied Math<br /><a href="mailto:kmcguin@uwo.ca">kmcguin@uwo.ca</a></p>
+						<p>Hi - I'm Killian McGuinness, and I'm the VP Academic of CSUS. I currently study Computer Science and Applied Mathematics, and I'm passionate about learning, teaching, and research. This year, you can look forward to casual discussions with professors over dinner and exam study sessions. If you have any questions or suggestions, feel free to shoot me an email!</p>
+					</div>
+				</div>
+				<div class="exec-container">
+					<div class="exec">
+						<div class="exec-image-container"><img src="images/daniel.jpg" class="exec-image" /></div>
+						<h1>Daniel Pfingstgraef</h1>
+						<p class="exec-detail"><b>First-Year Rep</b><br />1st Year CS<br /><a href="mailto:dpfingst@uwo.ca">dpfingst@uwo.ca</a></p>
+						<p>Hi! My name is Daniel Pfingstgraef and I am this year's First Year Representative of CSUS! I look forward to speaking with as many of my fellow first year Computer Science students as I can as we progress through the year, and am proud to represent you on CSUS. Don't hesitate to reach out to me at any time with questions, comments or concerns.</p>
+					</div>
+				</div>
+			</section>
+			<section id="contact">
+				<h1>Contact Us</h1>
+				<p>Room 12, Middlesex College<br />Western University</p>
+				<p>Email: <a href="mailto:csus@uwo.ca">csus@uwo.ca</a><br />Facebook: <a href="http://www.facebook.com/westerncsus">/westerncsus</a></p>
+			</section>
+		</div>
+		<footer><div class="content">© 2015 Computer Science Undergraduate Society. All rights reserved. Page by Kyle Windsor.</div></footer>
+	</div>
+</body>
+</html>
